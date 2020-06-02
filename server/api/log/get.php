e6951a76fcd57c27cf663fcd4b8a7ecc99b3f5b7
Michael D'Attilo <39676645+TehFishey@users.noreply.github.com>
2020-06-01 20:22:07 -0400

    CORs header adjustments
    Replaced 'HTTP_ORIGIN' variables in API endpoint headers with 'HTTP_REFERER'. Note that this does NOT work with my development environment right now (@*&$#^@!#*), but it seems to be necessary to get the api to play nice with different browsers when hosted server-side (ugh).

--- server/api/log/get.php
+++ server/api/log/get.php
@@ -14,7 +14,7 @@
  * 
  **************************************************************************************/
 
-header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
+header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_REFERER']);
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET POST");
 header("Access-Control-Max-Age: 3600");
